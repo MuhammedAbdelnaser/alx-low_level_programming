deb82c04344c461f173a7c9bcabebcf34b6f3830
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-02-17 19:40:05 +0200

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (c = 'a'; c >= 'f'; c++)
+	{
+		putchar(c);
+	}
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar("\n");
+
+	return (0);
+}
