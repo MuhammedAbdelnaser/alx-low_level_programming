6aa0605b6a8bcc9cb099d766fa2efe7c2f062a2f
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-02-22 00:40:09 +0200

    betty styling

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,8 +11,8 @@ int i, sum = 0;
 
 for (i = 0; i < 1024; i++)
 {
-    if ((i % 5) == 0 || (i % 3) == 0)
-        sum += i;
+if ((i % 5) == 0 || (i % 3) == 0)
+sum += i;
 }
 printf("%d\n", sum);
 return (0);
