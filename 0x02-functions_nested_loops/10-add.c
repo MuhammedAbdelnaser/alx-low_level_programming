d50de33833534bb4edacd176eed587881f723014
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-02-22 00:27:46 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
