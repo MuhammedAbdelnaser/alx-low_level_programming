2b7aaf1deda59a9c7dd8242db214139fc669bcac
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-02-28 18:31:18 +0200

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i, j, k;
+
+i = 0;
+while (str[i] != '\0')
+{
+    i++;
+}
+if (i % 2 == 0)
+{
+    j = i / 2;
+}
+else
+{
+    j = (i + 1) / 2;
+}
+for (k = j; k < i; k++)
+{
+    _putchar(str[k]);
+}
+_putchar('\n');
+}
