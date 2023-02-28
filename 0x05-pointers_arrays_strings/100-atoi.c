c3fc1cf1d315f06e05bf3fc12129c93d4c315668
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-02-28 18:52:29 +0200

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,27 +7,21 @@
  */
 int _atoi(char *s)
 {
-int i, sign, num;
+	int i = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-i = 0;
-sign = 1;
-num = 0;
-
-while (s[i] != '\0')
-{
-if (s[i] == '-')
-{
-sign *= -1;
-}
-else if (s[i] >= '0' && s[i] <= '9')
-{
-num = num * 10 + (s[i] - '0');
-}
-else if (num > 0)
-{
-break;
-}
-i++;
-}
-return (num *sign);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+		i++;
+	}
+	return (num * sign);
 }
