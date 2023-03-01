ab3cd2094909b32646ab5dbcbcaebe88a5927989
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-01 20:12:16 +0200

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array : function reverse array
+ * @a: array
+ * @n: the number of elements of the array
+ * Return : void
+ */
+void reverse_array(int *a, int n)
+{
+int i,j;
+
+for (i = 0; i < (n - 1) / 2; i++)
+{
+j = a[i];
+a[i] = a [n - 1 - i];
+a[n - 1 - i] = j;
+}
+}
