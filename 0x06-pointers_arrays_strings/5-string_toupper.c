4f80a2c418541fb8b00f2318e9b79d9b684c8473
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-01 21:01:19 +0200

    repairs

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,15 @@
  */
 char *string_toupper(char *s)
 {
-	char *start = s;
+int i = 0;
 
-	while (*s)
-	{
-		if (*s >= 'a' && *s <= 'z')
-			*s -= 32;
-		s++;
-	}
-	return (start);
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+{
+*(s + i) -= 'a' - 'A';
+}
+i++;
+}
+return (s);
 }
