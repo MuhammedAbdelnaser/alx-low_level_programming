3fcd4ce54c46821156030039548d9d95920767b5
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-01 20:34:35 +0200

    capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,44 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: capitalized string
+ */
 char *cap_string(char *s)
 {
-    int i = 0, j;
-    char a[] = " \t\n,;.!?\"(){}";
+int i = 0;
 
-    while (*(s + i))
-    {
-        if (*(s + i) >= 'a' && *(s + i) <= 'z')
-        {
-            if (i == 0)
-                *(s + i) -= 'a' - 'A';
-            else
-            {
-                for (j = 0; j <= 12; j++)
-                {
-                    if (a[j] == *(s + i - 1))
-                        *(s + i) -= 'a' - 'A';
-                }
-            }
-        }
-        i++;
-    }
-    return (s);
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+{
+if (i == 0)
+{
+*(s + i) -= 'a' - 'A';
+}
+else if (*(s + i - 1) == ' ' || *(s + i - 1) == '\t' || *(s + i - 1) == '\n')
+{
+*(s + i) -= 'a' - 'A';
+}
+else if (*(s + i - 1) == ',' || *(s + i - 1) == ';' || *(s + i - 1) == '.')
+{
+*(s + i) -= 'a' - 'A';
+}
+else if (*(s + i - 1) == '!' || *(s + i - 1) == '?' || *(s + i - 1) == '"')
+{
+*(s + i) -= 'a' - 'A';
+}
+else if (*(s + i - 1) == '(' || *(s + i - 1) == ')' || *(s + i - 1) == '{')
+{
+*(s + i) -= 'a' - 'A';
+}
+else if (*(s + i - 1) == '}')
+{
+*(s + i) -= 'a' - 'A';
+}
+}
+i++;
+}
+return (s);
 }
