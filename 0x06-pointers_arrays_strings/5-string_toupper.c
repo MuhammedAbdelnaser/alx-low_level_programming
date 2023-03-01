1c2e3d290b8e4d9b19e6e80ea5494b0073398bdc
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-01 20:58:43 +0200

    repairs

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * string_toupper - change lowercase to uppercase
  * @s:string
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,3 +1,10 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @s: string
+ * Return: char
+ */
 char *leet(char *s)
 {
 int i = 0, j;
@@ -7,11 +14,13 @@ str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 while (s[i])
 {
 for (j = 0; j < 10; j++)
+{
 if (s[i] == str[j])
 {
 s[i] = subs[j];
 }
 i++;
+}
 }
 
 return (s);
