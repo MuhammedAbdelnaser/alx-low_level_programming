7ded0329d1af6ea1cdb0b78d67c850acaf8f21dc
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-01 19:47:52 +0200

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,8 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n && src[i]; i++)
+
+for (i = 0; i < n && *(src + i); i++)
 {
 *(dest + i) = *(src + i);
 for (;i < n; i++)
