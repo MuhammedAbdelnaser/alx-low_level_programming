4780f4895f7b18f4a3d14464da0ac5308a1af664
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-01 20:45:19 +0200

    Betty Styling

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,21 +7,21 @@
  */
 char *leet(char *s)
 {
-int i =0, j;
 
+int i =0, j;
 char *letters = "aAeEoOtTlL";
 char *numbers = "4433007711";
 
-while (*(s + i))
-{
-for (j = 0; j < 10; j++)
-{
-if (*(s + i) == *(letters + j))
-{
-*(s + i) = *(numbers + j);
-}
-}
-i++;
-}
-return (s);
+    while (*(s + i))
+    {
+        for (j = 0; j < 10; j++)
+    {
+        if (*(s + i) == *(letters + j))
+        {
+            *(s + i) = *(numbers + j);
+        }
+    }
+        i++;
+    }
+    return (s);
 }
