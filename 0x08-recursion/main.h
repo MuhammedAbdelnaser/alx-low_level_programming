62a48ca0f6004f13e44719ff91376d9c29758187
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-03-08 21:47:47 +0200

    repair main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
