e6d59c525bd302389503a3a04ebae544373be43e
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-04-02 00:08:21 +0200

    add a new node at beginning of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,31 +3,35 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node to the end of linked list
- * @head: double pointer to a linked list
- * @str: string to add to the new node
- *
- * Return: pointer to the new node
- */
+* add_node_end - adds a new node to the end of linked list
+* @head: double pointer to a linked list
+* @str: string to add to the new node
+*
+* Return: pointer to the new node
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *add, *tmp;
+list_t *add, *tmp;
 
-	add = malloc(sizeof(list_t));
-	if (add == NULL)
-		return (NULL);
-	add->str = strdup(str);
+add = malloc(sizeof(list_t));
+if (add == NULL)
+{
+return (NULL);
+}
+add->str = strdup(str);
 
-	add->len = strlen(str);
-	add->next = NULL;
-	if (*head == NULL)
-	{
-		*head = add;
-		return (add);
-	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = add;
-	return (add);
+add->len = strlen(str);
+add->next = NULL;
+if (*head == NULL)
+{
+*head = add;
+return (add);
+}
+tmp = *head;
+while (tmp->next) 
+{
+tmp = tmp->next;
+}
+tmp->next = add;
+return (add);
 }
